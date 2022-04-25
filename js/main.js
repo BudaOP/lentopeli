1026f70fded2430a7979f6c854731c99d7903063
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2022-04-26 01:28:49 +0300

    v2

--- js/main.js
+++ js/main.js
@@ -1,152 +1,121 @@
 'use strict';
-(async function () {
-  // Käytetään leaflet.js -kirjastoa näyttämään sijainti kartalla (https://leafletjs.com/)
-  const map = L.map('map');
-  L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-  }).addTo(map);
-  map.setView([60, 24], 7);
+const player = prompt('Player name');
+// Käytetään leaflet.js -kirjastoa näyttämään sijainti kartalla (https://leafletjs.com/)
+const map = L.map('map', { tap: false });
+L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+}).addTo(map);
+map.setView([60, 24], 7);
 
-  // ikonit
-  const sininenIkoni = L.divIcon({ className: 'sininen-ikoni' });
-  const vihreaIkoni = L.divIcon({ className: 'vihrea-ikoni' });
-  const harmaaIkoni = L.divIcon({ className: 'harmaa-ikoni' });
+let currentAirport = {};
+let airportMarkers = L.featureGroup().addTo(map);
+const goals = [];
 
-  async function haeKentat() {
-    const vastaus = await fetch('airports.json');
-    const gameData = await vastaus.json();
-    return gameData;
-  }
+// ikonit
+const sininenIkoni = L.divIcon({ className: 'sininen-ikoni' });
+const vihreaIkoni = L.divIcon({ className: 'vihrea-ikoni' });
+const harmaaIkoni = L.divIcon({ className: 'harmaa-ikoni' });
 
-  function lisaaMarker(kentta) {
-    return L.marker([kentta.latitude, kentta.longitude]).addTo(map);
-  }
+async function haeKentat(url) {
+  const vastaus = await fetch('http://127.0.0.1:5000/' + url);
+  const gameData = await vastaus.json();
+  return gameData;
+}
 
-  async function haeSaatiedot(id) {
-    const vastaus = await fetch('weather.json');
-    const tiedot = await vastaus.json();
-    return tiedot;
-  }
+function lisaaMarker(kentta) {
+  return L.marker([kentta.latitude, kentta.longitude]).addTo(map);
+}
 
-  function naytaSaatiedot(kentta, weather = kentta.weather) {
-    console.log(kentta);
-    document.querySelector('#selected-name').innerHTML = name;
-    document.querySelector('#selected-temp').innerHTML = `${weather.temp}°C`;
-    document.querySelector('#selected-icon').src = weather.icon;
-    document.querySelector('#selected-weather').innerHTML = weather.description;
-  }
-
-  let currentAirport = {};
-  let airportMarkers = {};
-
-  const gameData = await haeKentat();
-
-  async function init() {
-    for (let kentta of gameData.location) {
-      const marker = lisaaMarker(kentta);
-      switch (kentta.status) {
-        case 0:
-          marker.setIcon(sininenIkoni);
-          break;
-        case 1:
-          marker.setIcon(vihreaIkoni);
-          break;
-        case 2:
-          marker.setIcon(harmaaIkoni);
-          break;
-      }
-      airportMarkers[kentta.ident] = marker;
-      if (kentta.status === 1) {
-        currentAirport = kentta;
-        document.querySelector('#selected-name').innerHTML =
-          'Start: ' + kentta.name;
-        map.flyTo([kentta.latitude, kentta.longitude], 12);
-        marker.bindPopup(`Olet täällä: ${kentta.name}`).openPopup();
-        // console.log(kentta);
-        naytaSaatiedot(kentta);
-        kentta.start = false;
-      } else {
-        const popupContent = document.createElement('div');
-        const h4 = document.createElement('h4');
-        h4.innerHTML = kentta.name;
-        popupContent.appendChild(h4);
-        if (kentta.status !== 2) {
-          const goNappi = L.DomUtil.create('button', 'nappi');
-          goNappi.innerHTML = 'Lennä tänne';
-          goNappi.addEventListener('click', async function () {
-            naytaSaatiedot(kentta);
-            currentAirport.status = 2;
-            currentAirport = kentta.ident;
-            kentta.status = 1;
-            init();
-          });
-
-          popupContent.appendChild(goNappi);
-        }
-        marker.bindPopup(popupContent);
-        marker.on('click', function () {
-          const etaisyys = distance(
-            [currentAirport.latitude, currentAirport.longitude],
-            [kentta.latitude, kentta.longitude]
-          );
-          const p = document.createElement('p');
-          p.innerHTML = `Etäisyys: ${etaisyys}km`;
-          popupContent.appendChild(p);
-          const dummyWeather = {
-            temp: '?',
-            description: '?',
-            icon: 'https://placekitten.com/50/50',
-            wind: {
-              speed: '?',
-              deg: '?',
-            },
-          };
-          naytaSaatiedot(kentta, dummyWeather);
-        });
+function naytaSaatiedot(kentta) {
+  const { name, weather } = kentta;
+  document.querySelector('#selected-name').innerHTML = `Weather at ${name}`;
+  document.querySelector('#selected-temp').innerHTML = `${weather.temp}°C`;
+  document.querySelector('#selected-icon').src = weather.icon;
+  document.querySelector(
+    '#selected-wind'
+  ).innerHTML = `${weather.wind.speed}m/s`;
+  document.querySelector('#selected-weather').innerHTML = weather.description;
+  if (weather.meets_goals.length > 0) {
+    let splash = false;
+    for (let goal of weather.meets_goals) {
+      if (!goals.includes(goal)) {
+        splash = true;
       }
     }
-    // console.log(airportMarkers, currentAirport);
-    const goalImgs = {};
-    document.querySelector('#goals').innerHTML = '';
-    for (let goal of gameData.goals) {
-      const li = document.createElement('li');
-      const figure = document.createElement('figure');
-      const img = document.createElement('img');
-      img.src = goal.icon;
-      img.alt = `goal name: ${goal.name}`;
-      goalImgs[goal.name] = img;
-      const figcaption = document.createElement('figcaption');
-      figcaption.innerHTML = goal.description;
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-      li.appendChild(figure);
-      document.querySelector('#goals').append(li);
+    if (splash) {
+      document.querySelector('.leima').classList.toggle('hide');
+      location.href = '#goals';
     }
   }
+}
 
-  init();
-
-  function distance(loc1, loc2) {
-    console.log(loc1, loc2);
-    const rad_per_deg = Math.PI / 180;
-    const rkm = 6371;
-
-    const dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg;
-    const dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg;
-
-    const latlon1 = loc1.map(function (i) {
-      return i * rad_per_deg;
-    });
-    const latlon2 = loc2.map(function (i) {
-      return i * rad_per_deg;
-    });
-
-    const a =
-      Math.sin(dlat_rad / 2) ** 2 +
-      Math.cos(latlon1[0]) * Math.cos(latlon2[0]) * Math.sin(dlon_rad / 2) ** 2;
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return Math.round(rkm * c);
+async function init(url = `newgame?player=${player}&loc=EFHK`) {
+  airportMarkers.clearLayers();
+  const gameData = await haeKentat(url);
+  if (gameData.status.co2.budget <= 0) {
+    alert('game over');
   }
-})();
+  document.querySelector('#player').innerHTML = gameData.status.name;
+  document.querySelector('#consumed').innerHTML = gameData.status.co2.consumed;
+  document.querySelector('#budget').innerHTML = gameData.status.co2.budget;
+  for (let kentta of gameData.location) {
+    const marker = lisaaMarker(kentta);
+    airportMarkers.addLayer(marker);
+    if (kentta.active) {
+      marker.setIcon(vihreaIkoni);
+      currentAirport = kentta;
+      map.flyTo([kentta.latitude, kentta.longitude], 10);
+      marker.bindPopup(`Olet täällä: ${kentta.name}`).openPopup();
+      console.log(document.querySelector('#selected-name'));
+      naytaSaatiedot(kentta);
+      kentta.start = false;
+    } else {
+      marker.setIcon(sininenIkoni);
+      const popupContent = document.createElement('div');
+      const h4 = document.createElement('h4');
+      h4.innerHTML = kentta.name;
+      popupContent.appendChild(h4);
+      const goNappi = L.DomUtil.create('button', 'nappi');
+      goNappi.innerHTML = 'Fly here';
+      goNappi.addEventListener('click', async function () {
+        const url = `flyto?game=${gameData.status.id}&dest=${kentta.ident}&consumption=${kentta.co2_consumption}`;
+        init(url);
+      });
+      popupContent.appendChild(goNappi);
+      marker.bindPopup(popupContent);
+      marker.on('click', function () {
+        console.log('täh');
+        const p = document.createElement('p');
+        p.innerHTML = `Distance: ${kentta.distance}km`;
+        popupContent.appendChild(p);
+        marker.openPopup();
+      });
+    }
+  }
+  document.querySelector('#goals').innerHTML = '';
+  for (let goal of gameData.goals) {
+    const li = document.createElement('li');
+    if (goal.reached) {
+      li.classList.add('done');
+      goals.push(goal.goalid);
+    }
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    img.src = goal.icon;
+    img.alt = `goal name: ${goal.name}`;
+    const figcaption = document.createElement('figcaption');
+    figcaption.innerHTML = goal.description;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    li.appendChild(figure);
+    document.querySelector('#goals').append(li);
+  }
+  map.invalidateSize(true);
+}
+
+document.querySelector('.leima').addEventListener('click', function () {
+  this.classList.toggle('hide');
+});
+
+init();
